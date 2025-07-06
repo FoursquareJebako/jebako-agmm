61b20e01b3fdd38324ecf4f70bffe771ce3ed1d4
bukunmikuti <bukunmikuti@gmail.com>
2025-07-06 10:22:14 +0100

    remove baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,6 @@ export default defineNuxtConfig({
   },
 
   app: {
-    baseURL: "/agmm/",
     head: {
       meta: [
         { name: "viewport", content: "width=device-width, initial-scale=1" },
