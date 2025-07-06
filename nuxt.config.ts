89dfda43c8864d0f11415d1d77a2f9820d8d57ca
bukunmikuti <bukunmikuti@gmail.com>
2025-07-06 10:30:48 +0100

    add baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,7 @@ export default defineNuxtConfig({
   },
 
   app: {
+    baseURL: "/agmm/",
     head: {
       meta: [
         { name: "viewport", content: "width=device-width, initial-scale=1" },
